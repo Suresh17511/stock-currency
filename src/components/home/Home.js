4b28cc9a76e547d3e7ec7aefefd3d58a26029c88
Suresh17511 <suresh.chidurala17511@gmail.com>
2021-06-25 00:28:50 +0530

    cors problem

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -84,18 +84,10 @@ const Home = () => {
       );
     });
 
-  const config = {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-  };
-
   useEffect(() => {
     axios
       .get(
-        'https://api.nomics.com/v1/currencies/ticker?key=c692ca1e205a0d3f84ef917abe3168f7996588ff&per-page=100',
-        config
+        'https://api.nomics.com/v1/currencies/ticker?key=c692ca1e205a0d3f84ef917abe3168f7996588ff&per-page=100'
       )
       .then((res) => {
         setC_coins(res.data);
